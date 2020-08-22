b1644a9733d231d470411e09c00bf2c2370b8fda
Josiah Harding <hardingjosiah@inaburrastudents.nsw.edu.au>
2020-08-22 10:55:06 +1000

    learning version control

--- src/setup/initialise.py
+++ src/setup/initialise.py
@@ -1,7 +1,10 @@
-def validateSize(size):
-    # We need to validate that the size we are using is a perfect square
-    return size
+import math
 
-def getBoard(size):
+def validate_size(size):
+    # We need to validate that the size we are using is a perfect square
+    root = math.sqrt(size)
+    return int(root) ** 2
+
+def get_board(size):
     # we need to construct the board to use with points and possible values
     return
--- src/setup/test_.py
+++ src/setup/test_.py
@@ -1,10 +1,10 @@
 from . import initialise
 
 def test_validateSize():
-    assert initialise.validateSize(4) == 4
-    assert initialise.validateSize(9) == 9
-    assert initialise.validateSize(16) == 16
-    assert initialise.validateSize(15) == 9
-    assert initialise.validateSize(16.5) == 16
-    assert initialise.validateSize(-9) == 9
-    assert initialise.validateSize(-15) == 9
+    assert initialise.validate_size(4) == 4
+    assert initialise.validate_size(9) == 9
+    assert initialise.validate_size(16) == 16
+    assert initialise.validate_size(15) == 9
+    assert initialise.validate_size(16.5) == 16
+    assert initialise.validate_size(-9) == 9
+    assert initialise.validate_size(-15) == 9
